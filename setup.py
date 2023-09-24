67af09a1f8469bf68ead8e3280fb0d6f6a16379e
Uday Zope <31612865+udayzee05@users.noreply.github.com>
2023-09-24 16:03:36 +0530

    template created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ObjectDetection',
+    version= '0.0.0',
+    author= 'Uday Bhaskar Zope',
+    author_email= 'udayzee05@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
